abae73f96f050b214512696979021ed66ad00b65
Landon Harnish <28lharnish@live.ytech.edu>
2025-03-04 11:19:43 -0500

    Begun work on portfolio page

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-//* Change Variable
-let styleMode = document.cookie;
-
-//* Change Variable
-let styleTag = document.getElementById("styleSheet");
-
-//* Conditional Branch + Else Branch
-if (styleMode == "dark") {
-    styleTag.href = "./css/dark.css";
-    document.cookie = "dark";
-} else {
-    styleTag.href = "./css/light.css";
-    document.cookie = "light";
-}
-
-//* Common Event
-function switchMode() {
-    if (styleMode == "dark") {
-        styleTag.href = "./css/light.css";
-        document.cookie = "light";
-        styleMode = "light";
-    } else {
-        styleTag.href = "./css/dark.css";
-        document.cookie = "dark";
-        styleMode = "dark";
-    }
-}
-
-function showHide() {
-    let toolBarButtons = document.getElementById("buttons");
-    
-    if(toolBarButtons.style.display === 'block') {
-         toolBarButtons.style.display = 'none'
-    } else {
-         toolBarButtons.style.display = 'block'
-    }
+window.onload = function() {
+    document.getElementById('nameText').style.opacity = 1;
 }

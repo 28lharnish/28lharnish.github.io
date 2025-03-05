904991fdd78c77962b82979f529da85b54dc4c0b
Landon Harnish <28lharnish@live.ytech.edu>
2025-03-05 14:59:05 -0500

    Start page

---
+++ about/sources/js/main.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    document.getElementById('convo1').style.opacity = 1;
+    document.getElementById('convo2').style.opacity = 1;
+    document.getElementById('convo3').style.opacity = 1;
+    document.getElementById('convo-dot1').style.opacity = 1;
+    document.getElementById('convo-dot2').style.opacity = 1;
+    document.getElementById('convo-dot3').style.opacity = 1;
+
+    document.getElementById('conversation').style.opacity = 0;
+}
---
+++ sources/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById('nameText').style.opacity = 1;
+    document.getElementById('nameSubtext').style.opacity = 1;
+    document.getElementById('logoIMG').style.opacity = 1;
+    
+    document.getElementById('learnMore').style.opacity = 1;
+}

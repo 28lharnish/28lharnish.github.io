ae4b953ac942b9105065ae00916051f7659021b2
Landon Harnish <28lharnish@live.ytech.edu>
2025-03-06 09:09:06 -0500

    Push Updates

--- about/sources/js/main.js
+++ about/sources/js/main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    document.getElementsByTagName('main')[0].scrollTo(0, 0);
     document.getElementById('convo1').style.opacity = 1;
     setTimeout(() => document.getElementById('convo2').style.opacity = 1, 1000);
     setTimeout(() => document.getElementById('convo3').style.opacity = 1, 2000);
@@ -7,16 +8,18 @@ window.onload = function() {
     setTimeout(() => document.getElementById('convo-dot1').style.opacity = 1, 3500);
 
     setTimeout(() => document.getElementById('conversation').style.opacity = 0, 5000);
-    setTimeout(() => document.getElementById('aboutMe').style.display = 'flex', 6000);
-    setTimeout(() => document.getElementById('aboutMe').style.opacity = 1, 6000)
-    setTimeout(() => document.getElementById('skipIntro').style.opacity = 0, 6000)
-    setTimeout(() => document.getElementById('skipIntro').remove(), 6000)
+    setTimeout(() => document.getElementById('aboutMe').style.display = 'flex', 4000);
+    setTimeout(() => document.getElementById('aboutMe').style.opacity = 1, 6000);
+    try {
+        setTimeout(() => document.getElementById('skipIntro').style.opacity = 0, 6000)
+        setTimeout(() => document.getElementById('skipIntro').remove(), 6000)
+    } catch(e) {}
 }
 
 function skip() {
     document.getElementById('conversation').style.opacity = 0;
     document.getElementById('aboutMe').style.display = 'flex';
-    document.getElementById('aboutMe').style.opacity = 1;
+    setTimeout(() => document.getElementById('aboutMe').style.opacity = 1, 500);
     document.getElementById('skipIntro').style.opacity = 0;
     document.getElementById('skipIntro').remove() 
 }
